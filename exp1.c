bddc9d3f6d30ce8a9247bdec6ad2435d5a446022
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-08-27 11:37:33 +0530

    commit

--- exp1.c
+++ exp1.c
@@ -3,13 +3,13 @@
 #include<stdio.h>
 int main()
 { 
-    float side1,side2,side3;
+    int side1,side2,side3;
     printf("enter side a of triangle:");
-    scanf("%f",side1);
+    scanf("%d",&side1);
     printf("enter side b of triangle:");
-    scanf("%f",side2);
+    scanf("%d",&side2);
     printf("enter side c of triangle:");
-    scanf("%f",side3);
+    scanf("%d",&side3);
     if ((side1==side2) && (side2==side3))
     {
         printf("it is an equilateral triangle");
@@ -23,6 +23,7 @@ int main()
     {
         printf("it is a scalene triangle");
     }
+    return 0;
 }
     
     
