8f500d1ff4a970f0684bc80058bc982cb0ed814e
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-10 10:44:42 +0530

    calender

--- calender.c
+++ calender.c
@@ -4,5 +4,33 @@
 #include <stdio.h>
 int main()
 {
-    int year;
+    int year, days=0, day;
+    printf("enter year: ");
+    scanf("%d", &year);
+    for(int i=1; i<year; i++)
+    {
+        if (i%400==0 || i%4==0 && i%100!=0)
+            days+=366;
+        else
+            days+=365;
+    }
+    day= (days+1)%7;
+    printf("1st january %d is a ", year);
+    switch(day){
+        case 0: printf("sunday\n");
+        break;
+        case 1: printf("monday\n");
+        break;
+        case 2: printf("tuesday\n");
+        break;
+        case 3: printf("wednesday\n");
+        break;
+        case 4: printf("thursday\n");
+        break;
+        case 5: printf("friday\n");
+        break;
+        case 6: printf("saturday\n");
+        break;
+    }
+    return 0;
 }
